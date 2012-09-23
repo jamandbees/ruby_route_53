ccc71c47fc4667c6281ee2e43a038688a49a435a
Jam <jam@jamandbees.net>
2012-09-22 18:17:02 -0600

    Started using minitest. Added testconnection class.

--- test/test_route53.rb
+++ test/test_route53.rb
@@ -1,3 +1,14 @@
-require "test/unit"
-
+require "minitest/autorun"
+require "minitest/pride"
 require_relative "../lib/route53"
+
+class TestConnection < MiniTest::Unit::TestCase
+
+    def setup
+        @connection = Route53::Connection.new 1, 1
+    end
+
+	def test_get_date
+		assert @connection.get_date
+	end
+end
